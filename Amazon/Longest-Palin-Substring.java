9ae5548ab8344aa128436a329ebf1e655c55d52a
Ayush Kiledar <88903550+Ayush-2019@users.noreply.github.com>
2023-10-15 16:07:22 -0400

    Create Longest-Palin-Substring.java
    Given a string S, find the longest palindromic substring in S. Substring of string S: S[ i . . . . j ] where 0 ≤ i ≤ j < len(S). Palindrome string: A string which reads the same backwards. More formally, S is palindrome if reverse(S) = S. Incase of conflict, return the substring which occurs first ( with the least starting index ).
    
    
    Example 1:
    
    Input:
    S = "aaaabbaa"
    Output:
    aabbaa
    Explanation:
    The longest palindrome string present in
    the given string is "aabbaa".
    
    Your Task:  
    You don't need to read input or print anything. Your task is to complete the function longestPalindrome() which takes string S as input parameters and returns longest pallindrome present in string.
    
    
    Expected Time Complexity: O(|S|2)
    Expected Auxiliary Space: O(1)
    
    
    Constraints:
    1 ≤ |S| ≤ 104

---
+++ Amazon/Longest-Palin-Substring.java
@@ -0,0 +1,35 @@
+class Solution{
+    
+    boolean isPalin(boolean dp[][], String s, int i, int j){
+        if(i == j) return dp[i][j] = true;
+        
+        else if(j-i == 1 && s.charAt(i) == s.charAt(j)) return dp[i][j] = true;
+        
+        else if(s.charAt(i) == s.charAt(j) && dp[i+1][j-1] == true) return dp[i][j] = true;
+        
+        return false;
+    }
+    
+    String longestPalindrome(String s){
+        // code here
+        int start = 0, max = 0, n = s.length();
+        boolean dp[][] = new boolean[n][n];
+        
+        for(int i=0;i<n;i++) Arrays.fill(dp[i], false);
+        
+        
+        for(int g=0; g<n;g++){
+            
+            for(int i=0, j=g; j<n;i++, j++){
+                if(isPalin(dp, s, i, j)){
+                    if(j-i+1>max){
+                        start = i;
+                        max = j-i+1;
+                    }
+                }
+            }
+        }
+        
+        return s.substring(start, max);
+    }
+}
