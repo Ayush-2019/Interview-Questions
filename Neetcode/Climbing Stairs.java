0803bfd4e727a0cd66a0d5f902f2871296032ec0
Ayush Kiledar <88903550+Ayush-2019@users.noreply.github.com>
2023-10-25 19:32:26 -0400

    Update Climbing Stairs.java
    You are climbing a staircase. It takes n steps to reach the top.
    
    Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
    
     
    
    Example 1:
    
    Input: n = 2
    Output: 2
    Explanation: There are two ways to climb to the top.
    1. 1 step + 1 step
    2. 2 steps
    Example 2:
    
    Input: n = 3
    Output: 3
    Explanation: There are three ways to climb to the top.
    1. 1 step + 1 step + 1 step
    2. 1 step + 2 steps
    3. 2 steps + 1 step
     
    
    Constraints:
    
    1 <= n <= 45

--- Neetcode/Climbing Stairs.java
+++ Neetcode/Climbing Stairs.java
@@ -18,5 +18,29 @@ class Solution {
 
         return dp[0];
 
+        
+        
+
+    }
+    // space optimization approach
+    public int climbStairs(int n) {
+        // int dp[] = new int[n];
+        // Arrays.fill(dp, -1);
+        // solve(dp,n,0);
+
+        // return dp[0];
+        if(n==1) return 1;
+        if(n==2) return 2;
+        int prev = 1;
+        int curr = 2;
+
+        for(int i=2;i<n;i++){
+            int temp = curr;
+            curr = prev+curr;
+            prev = temp;
+        }
+
+        return curr;
+
     }
 }
