16784b23d68697fbd8622229e44d95ec361ce3e2
Ayush Kiledar <88903550+Ayush-2019@users.noreply.github.com>
2023-12-21 23:05:56 -0500

    Create Jump Game.java
    You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
    
    Return true if you can reach the last index, or false otherwise.
    
     
    
    Example 1:
    
    Input: nums = [2,3,1,1,4]
    Output: true
    Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
    Example 2:
    
    Input: nums = [3,2,1,0,4]
    Output: false
    Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
     
    
    Constraints:
    
    1 <= nums.length <= 104
    0 <= nums[i] <= 105

---
+++ Neetcode/Jump Game.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+
+
+        int n = nums.length;
+
+        int[] range = new int[n];
+
+        for(int i=0;i<n;i++){
+
+            range[i] = nums[i] + i;
+        } 
+
+        int curr = 0, temp = Integer.MIN_VALUE;
+        if(curr == n-1) return true;
+        while(curr<n-1){
+            int temp2 = curr;
+            for(int i=curr+1;i<=temp2+nums[temp2];i++){
+                if(range[i-1]>=n-1) return true;
+                if(temp<=range[i]){
+                    temp = Math.max(temp, range[i]);
+                    curr = i;
+                }
+
+                
+
+            }
+            if(nums[curr] == 0) return false;
+
+        }
+
+        return true;
+        
+    }
+}
