a01d46115533fedd6f4239d088325af686d01c02
Ayush Kiledar <88903550+Ayush-2019@users.noreply.github.com>
2023-12-23 14:50:19 -0500

    Create Partition Labels.java
    You are given a string s. We want to partition the string into as many parts as possible so that each letter appears in at most one part.
    
    Note that the partition is done so that after concatenating all the parts in order, the resultant string should be s.
    
    Return a list of integers representing the size of these parts.
    
     
    
    Example 1:
    
    Input: s = "ababcbacadefegdehijhklij"
    Output: [9,7,8]
    Explanation:
    The partition is "ababcbaca", "defegde", "hijhklij".
    This is a partition so that each letter appears in at most one part.
    A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits s into less parts.
    Example 2:
    
    Input: s = "eccbbbbdec"
    Output: [10]
     
    
    Constraints:
    
    1 <= s.length <= 500
    s consists of lowercase English letters.

---
+++ Neetcode/Partition Labels.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for(int i=0;i<s.length();i++) map.put(s.charAt(i), i);
+
+        List<Integer> ans = new ArrayList<>();
+
+        int curr_limit = map.get(s.charAt(0)), c = 1;
+
+        for(int i=1;i<s.length();i++){
+
+            if(i<=curr_limit){
+                
+                curr_limit = Math.max(curr_limit, map.get(s.charAt(i)));
+                c++;
+            }
+            else{
+                curr_limit = map.get(s.charAt(i));
+                ans.add(c);
+                c = 1;
+            }
+
+        }
+        ans.add(c);
+        return ans;
+    }
+}
