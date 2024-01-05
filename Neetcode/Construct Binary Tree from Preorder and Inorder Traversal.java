61c6f41b3f6836ed8fbbfb3ffc7724e491aa919e
Ayush Kiledar <88903550+Ayush-2019@users.noreply.github.com>
2024-01-04 21:40:43 -0500

    Create Construct Binary Tree from Preorder and Inorder Traversal.java
    Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
    
     
    
    Example 1:
    
    
    Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
    Output: [3,9,20,null,null,15,7]
    Example 2:
    
    Input: preorder = [-1], inorder = [-1]
    Output: [-1]
     
    
    Constraints:
    
    1 <= preorder.length <= 3000
    inorder.length == preorder.length
    -3000 <= preorder[i], inorder[i] <= 3000
    preorder and inorder consist of unique values.
    Each value of inorder also appears in preorder.
    preorder is guaranteed to be the preorder traversal of the tree.
    inorder is guaranteed to be the inorder traversal of the tree.

---
+++ Neetcode/Construct Binary Tree from Preorder and Inorder Traversal.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public TreeNode solve(Map<Integer, Integer> map, int[] pre, int[] in, int preIndex, int inStart, int inEnd){
+
+        if(inStart > inEnd) return null;
+
+        TreeNode node = new TreeNode(pre[preIndex]);
+
+        int mid = map.get(pre[preIndex]);
+        int len = mid - inStart;
+
+        node.left = solve(map, pre, in, preIndex+1, inStart, mid-1);
+        node.right = solve(map, pre, in, preIndex+len+1, mid+1, inEnd);
+
+        return node;
+
+
+
+    }
+
+    public TreeNode buildTree(int[] pre, int[] in) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0;i<in.length;i++) map.put(in[i], i);
+
+        TreeNode root = solve(map, pre, in, 0, 0, in.length-1);
+
+        return root;
+        
+    }
+}
