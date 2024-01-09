8a02a4a8de4e827f46b0499dd338c888e6f85c07
Ayush Kiledar <88903550+Ayush-2019@users.noreply.github.com>
2024-01-08 21:46:25 -0500

    Create Word Search.java
    Given an m x n grid of characters board and a string word, return true if word exists in the grid.
    
    The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
    
     
    
    Example 1:
    
    
    Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
    Output: true
    Example 2:
    
    
    Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
    Output: true
    Example 3:
    
    
    Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
    Output: false
     
    
    Constraints:
    
    m == board.length
    n = board[i].length
    1 <= m, n <= 6
    1 <= word.length <= 15
    board and word consists of only lowercase and uppercase English letters.

---
+++ Neetcode/Word Search.java
@@ -0,0 +1,41 @@
+class Solution {
+
+    public boolean solve(char[][] board, String word, int index, int i, int j, boolean[][] vis){
+
+        if(index == word.length()) return true;
+
+        if(i<0 || j<0 || i>=board.length || j>= board[0].length || word.charAt(index) != board[i][j] || vis[i][j]) return false;
+
+        vis[i][j] = true;
+
+        boolean a = solve(board, word, index+1, i+1,j, vis) || 
+                    solve(board, word, index+1, i-1,j, vis) || 
+                    solve(board, word, index+1, i,j+1, vis) || 
+                    solve(board, word, index+1, i,j-1, vis);
+
+        vis[i][j] = false;
+
+        return a;
+
+    }
+
+    public boolean exist(char[][] board, String word) {
+
+        boolean[][] vis = new boolean[board.length][board[0].length];
+
+        
+
+        for(int i =0; i<board.length; i++)
+        {
+            for(int j =0; j < board[i].length; j++)
+            {
+                if(board[i][j] == word.charAt(0))
+                {
+                   if(solve(board, word, 0, i, j, vis)) return true;
+                }
+            }
+        }
+        return false;
+        
+    }
+}
